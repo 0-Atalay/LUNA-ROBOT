9f1d8d21d587ba843979f49e689a6b6a3ec620fb
Drmehmetaktass <87446203+Drmehmetaktass@users.noreply.github.com>
2021-08-11 16:53:32 +0300

    Update combot_antispam_system.py

--- tg_bot/modules/combot_antispam_system.py
+++ tg_bot/modules/combot_antispam_system.py
@@ -263,14 +263,10 @@ def getDefense(bot: Bot, update: Update):
 #     welcome = welcome.replace('$rules', '{rules}')
 #     sql.set_custom_welcome(chat_id, welcome, sql.Types.TEXT)
 ABOUT_CAS =  "<b>Combot Anti-Spam System (CAS)</b>" \
-
-            "\n\nCAS stands for Combot Anti-Spam, an automated system designed to detect spammers in Telegram groups."\
-
-            "\nIf a user with any spam record connects to a CAS-secured group, the CAS system will ban that user immediately."\
-
-            "\n\n<i>CAS bans are permanent, non-negotiable, and cannot be removed by Combot community managers.</i>" \
-
-            "\n<i>If a CAS ban is determined to have been issued incorrectly, it will automatically be removed.</i>"
+             "\n\nCAS stands for Combot Anti-Spam, an automated system designed to detect spammers in Telegram groups."\
+             "\nIf a user with any spam record connects to a CAS-secured group, the CAS system will ban that user immediately."\
+             "\n\n<i>CAS bans are permanent, non-negotiable, and cannot be removed by Combot community managers.</i>" \
+             "\n<i>If a CAS ban is determined to have been issued incorrectly, it will automatically be removed.</i>"
 
 @run_async
 def about_cas(bot: Bot, update: Update):
