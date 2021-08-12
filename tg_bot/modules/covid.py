b5e22345c9bb5f79ad2e81744c2b5d10a6b91038
Drmehmetaktass <87446203+Drmehmetaktass@users.noreply.github.com>
2021-08-12 10:08:16 +0300

    Update covid.py

--- tg_bot/modules/covid.py
+++ tg_bot/modules/covid.py
@@ -25,26 +25,26 @@ def covid(bot: Bot, update: Update):
         total_deaths_global = parsed["latest"]["deaths"]
         total_recovered_global = parsed["latest"]["recovered"]
         active_cases_covid19 = total_confirmed_global - total_deaths_global - total_recovered_global
-        reply_text = ("*Corona Stats🦠:*\n"
-        "Total Confirmed: `" + str(total_confirmed_global) + "`\n"
-        "Total Deaths: `" + str(total_deaths_global) + "`\n"
-        "Total Recovered: `" + str(total_recovered_global) +"`\n"
-        "Active Cases: `"+ str(active_cases_covid19) + "`")
+        reply_text = ("*Corona İstatistikleri🦠:*\n"
+        "Toplam Onaylanan: `" + str(total_confirmed_global) + "`\n"
+        "Toplam Ölüm: `" + str(total_deaths_global) + "`\n"
+        "Toplam Kurtarılan: `" + str(total_recovered_global) +"`\n"
+        "Aktif Vakalar: `"+ str(active_cases_covid19) + "`")
         message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
         return
 
     elif fetch.status_code == 404:
-        reply_text = "The API is currently down."
+        reply_text = "API şu anda çalışmıyor."
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
 
 
 __help__ = """
  
- - /covid get worldwide corona status
+ - /covid dünya çapında korona durumu almak
 """
 
-__mod_name__ = 'COVID-19'
+__mod_name__ = 'COVID-19🤧'
 
 COVID_HANDLER = CommandHandler("covid", covid, admin_ok=True)
 dispatcher.add_handler(COVID_HANDLER)
